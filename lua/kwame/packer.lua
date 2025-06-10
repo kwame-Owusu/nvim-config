27632d7ae25f20cb29e1c8ec61f880959b7874af
kwame-Owusu <alfredowusu595@gmail.com>
2025-06-10 22:04:06 +0100

    updated luasnip enabling js regexp

--- lua/kwame/packer.lua
+++ lua/kwame/packer.lua
@@ -35,7 +35,13 @@ return require('packer').startup(function(use)
 		  {'hrsh7th/cmp-nvim-lsp'},         -- LSP source for nvim-cmp
 	  }
   }
-  use {'L3MON4D3/LuaSnip'}
   use {'saadparwaiz1/cmp_luasnip'}
+  use({
+	  "L3MON4D3/LuaSnip",
+	  -- follow latest release.
+	  tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	  -- install jsregexp (optional!:).
+	  run = "make install_jsregexp"
+  })
 
   end)
