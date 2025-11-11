bb7a65842d32471f2b021bb2ca324bdc638dc31f
kwame-Owusu <alfredowusu595@gmail.com>
2025-11-11 20:44:56 +0000

    update nvim tree bindings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,41 +1,42 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    vim.opt.termguicolors = true
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        vim.opt.termguicolors = true
 
-    require("nvim-tree").setup({
-      renderer = {
-        highlight_git = true,
-        highlight_opened_files = "none",
-        root_folder_modifier = ":~",
-      },
-      view = {
-        side = "left",
-        width = 30,
-      },
-      filters = {
-        dotfiles = true,
-      },
-      git = {
-        enable = true,
-        ignore = false,
-      },
-    })
+        require("nvim-tree").setup({
+            renderer = {
+                highlight_git = true,
+                highlight_opened_files = "none",
+                root_folder_modifier = ":~",
+            },
+            view = {
+                side = "left",
+                width = 30,
+            },
+            filters = {
+                dotfiles = true,
+            },
+            git = {
+                enable = true,
+                ignore = false,
+            },
+        })
 
-    -- Transparent background
-    vim.cmd([[
+        -- Transparent background
+        vim.cmd([[
       highlight NvimTreeNormal guibg=NONE ctermbg=NONE
       highlight NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE
       highlight NvimTreeVertSplit guibg=NONE ctermbg=NONE
     ]])
 
-    vim.keymap.set("n", "<C-n>", ":NvimTreeFindFileToggle<CR>")
-  end,
+        vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
+        vim.keymap.set("n", "<leader>fe", "<cmd>NvimTreeFindFile<CR>", { desc = "Find current file in tree" })
+    end,
 }
