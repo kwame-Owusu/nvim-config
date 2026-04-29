730f2d02ad11e49084e6aaa0bc8651920260a850
kwame-Owusu <alfredowusu595@gmail.com>
2026-04-29 11:35:45 +0100

    added themery.nvim and new colorschemes

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
+vim.opt.termguicolors = true
+
 require("kwame.lazy")
 require("kwame")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,31 +1,59 @@
 return {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,    -- Load immediately during startup
-    priority = 1000, -- Load before other plugins
-    config = function()
-        require("gruvbox").setup({
-            terminal_colors = true, -- add neovim terminal colors
-            undercurl = true,
-            underline = true,
-            bold = false,
-            italic = {
-                strings = true,
-                emphasis = true,
-                comments = true,
-                operators = false,
-                folds = true,
-            },
-            strikethrough = true,
-            invert_selection = false,
-            invert_signs = false,
-            invert_tabline = false,
-            inverse = true, -- invert background for search, diffs, statuslines and errors
-            contrast = "",  -- can be "hard", "soft" or empty string
-            palette_overrides = {},
-            overrides = {},
-            dim_inactive = false,
-            transparent_mode = true,
-        })
-        vim.cmd("colorscheme gruvbox")
-    end,
+    -- not using vim.cmd("colorscheme <theme_name>")
+    -- because themery.nvim will take care of that
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = true,
+                undercurl = true,
+                underline = true,
+                bold = false,
+                italic = {
+                    strings = true,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                inverse = true,
+                contrast = "",
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = true,
+            })
+        end,
+    },
+
+    {
+        "oskarnurm/koda.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("koda").setup({
+                transparent = false,
+                theme = { dark = "dark" }
+            })
+        end,
+    },
+
+    {
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("everforest").setup({
+                background = "hard", -- options: "low", "medium", "hard"
+                transparent_background_level = 1,
+            })
+        end,
+    },
 }
