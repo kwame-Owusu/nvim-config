71a6ed1605b1d32aa26d60b0f71cce0da93bb8aa
kwame-Owusu <alfredowusu595@gmail.com>
2026-05-18 20:36:32 +0100

    add variations for rose-pine

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -3,7 +3,37 @@ return {
     lazy = false,
     config = function()
         require("themery").setup({
-            themes = { "gruvbox", "koda", "everforest", "oh-lucy", "poimandres", "rose-pine" },
+            themes = {
+                "gruvbox",
+                "koda",
+                "everforest",
+                "oh-lucy",
+                "poimandres",
+                -- Rose Pine Main Variant
+                {
+                    name = "Rose Pine Main",
+                    colorscheme = "rose-pine",
+                    before = [[
+                        require("rose-pine").setup({
+                            variant = "main",
+                            extend_background_behind_borders = true,
+                            styles = { bold = false, italic = false, transparency = false },
+                        })
+                    ]],
+                },
+                -- Rose Pine Moon Variant
+                {
+                    name = "Rose Pine Moon",
+                    colorscheme = "rose-pine",
+                    before = [[
+                        require("rose-pine").setup({
+                            variant = "moon",
+                            extend_background_behind_borders = true,
+                            styles = { bold = false, italic = false, transparency = false },
+                        })
+                    ]],
+                },
+            },
             livePreview = true,
         })
     end
