3ad9478d644cb46038cbf053e69916cf9d3ce317
kwame-Owusu <alfredowusu595@gmail.com>
2026-05-27 20:22:02 +0100

    fix: fix the breaking changes with nvim 0.12.2, update telescope and tresitter configs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-telescope/telescope.nvim",
-    version = "0.1.8",
+    branch = "master",
     dependencies = { "nvim-lua/plenary.nvim" },
 
     config = function()
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,20 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    lazy = false,
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- Parsers to install
-            ensure_installed = {
-                "c",
-                "javascript",
-                "typescript",
-                "lua",
-                "vim",
-                "html",
-                "vimdoc",
-                "query",
-                "markdown",
-                "markdown_inline",
-                "go",
-                "python",
-                "css",
-                "json",
-                "toml",
-                "yaml",
-                "tsx",
-                "rust",
-                "astro",
-            },
+        local parsers = {
+            "c", "javascript", "typescript", "lua", "vim",
+            "html", "vimdoc", "query", "markdown", "markdown_inline",
+            "go", "python", "css", "json", "toml", "yaml",
+            "tsx", "rust", "astro",
+        }
 
-            -- Install parsers synchronously
-            sync_install = false,
-            auto_install = false,
-
-            highlight = {
-                enable = true,
-                -- Set to true if you want both TS and Vim regex highlighting
-                additional_vim_regex_highlighting = false,
-            },
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = parsers,
+            callback = function() vim.treesitter.start() end,
         })
+
+        require('nvim-treesitter').install(parsers)
     end,
 }
