e3598605283efffd7be5dca46f40fc1426fdc994
kwame-Owusu <alfredowusu595@gmail.com>
2026-07-04 17:37:49 +0100

    add remap for allowing multiple indents in visual mode

--- lua/kwame/remap.lua
+++ lua/kwame/remap.lua
@@ -4,6 +4,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- allow multiple indents in visual mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
